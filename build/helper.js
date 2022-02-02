9fffa59a43c5cdf8c0825d4e4194efd368e15e6b
Sébastien Le Maire <sebastien.le-maire@sooyoos.com>
2022-02-02 14:48:06 +0100

    Remove build from repo

--- build/helper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNumeric = void 0;
-const isNumeric = (num) => num.length > 0 && !isNaN(+num);
-exports.isNumeric = isNumeric;
